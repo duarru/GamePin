fae36f0b13bbfb4d3ae29e85ff7fa3c4419a3323
Obedis Duarte Arruda <duarru31@gmail.com>
2020-07-21 20:42:37 -0300

    Finishing Touches

--- Assets/Scripts/CircleRotateScript.cs
+++ Assets/Scripts/CircleRotateScript.cs
@@ -11,9 +11,23 @@ public class CircleRotateScript : MonoBehaviour
     /// <summary>Awake is called when the script instance is begin loaded</summary>
     void Awake()
     {
-        canRotate = true;   
+        canRotate = true;
+        StartCoroutine(ChangeRotation());
     }
 
+    IEnumerator ChangeRotation()
+    {
+        yield return new WaitForSeconds(2f);
+        if (Random.Range(0,2) > 0)
+        {
+            rotationSpeed = -Random.Range(50, 100);
+        }
+        else
+        {
+            rotationSpeed = Random.Range(50, 100);
+        }
+        StartCoroutine(ChangeRotation());
+    }
     // Update is called once per frame
     void Update()
     {
